028c79d430ef1224317db95220faaf25bfab53bf
zidan <zidanahammed444@gmail.com>
2025-12-30 09:58:19 +0530

    dash

--- frontend/src/MainRouter.jsx
+++ frontend/src/MainRouter.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from "react-router-dom"
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
+import CustomerDashboard from "./pages/dashboard/CustomerDashboard.jsx";
 
 function MainRouter() {
   return (
@@ -11,6 +12,7 @@ function MainRouter() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/dashboard/customer" element={<CustomerDashboard/>}/>
     </Routes>
     </>
   )
